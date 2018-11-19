cb9ce80e9271ccd303ccd8096fab8e998dd77fa0
Sidath Asiri <sidathasiri@gmail.com>
2018-11-19 10:25:38 +0530

    creating large random file added

--- src/main/java/com/newgen/springbootrest/controller/FileController.java
+++ src/main/java/com/newgen/springbootrest/controller/FileController.java
@@ -15,15 +15,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.util.*;
 
 @RestController
 @RequestMapping("/files")
@@ -45,6 +42,20 @@ public class FileController {
 
     @RequestMapping(path = "/download", method = RequestMethod.GET)
     public ResponseEntity<Resource> download(@RequestParam(value="name") String name) throws IOException {
+        Random rand = new Random();
+        int fileSize = (2 + rand.nextInt(8))*1024*1024;
+        System.out.println(fileSize);
+        char[] chars = new char[fileSize];
+        Arrays.fill(chars, 'a');
+
+        String writingStr = new String(chars);
+
+        //random file
+        BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Sidath\\IdeaProjects\\spring-boot-rest\\src\\main\\resources\\static\\downloading_files\\"+name+".txt"));
+        writer.write(writingStr);
+
+        writer.close();
+
         HttpHeaders headers = new HttpHeaders();
         String headerValue = "attachment; filename="+name+".txt";
         headers.add(HttpHeaders.CONTENT_DISPOSITION, headerValue);
@@ -54,6 +65,7 @@ public class FileController {
 
         Path path = Paths.get(file.getAbsolutePath());
         ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
         return ResponseEntity.ok()
                 .headers(headers)
                 .contentLength(file.length())
